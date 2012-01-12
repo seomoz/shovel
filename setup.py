b923b992f0b04b88a323a08b4b11e0e8687efeb8
Dan Lecocq <dan@seomoz.org>
2012-01-12 15:18:56 -0800

    Version bump.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 	}
 
 setup(name           = 'shovel',
-	version          = '0.1.0',
+	version          = '0.1.1',
 	description      = 'Not Rake, but Shovel',
 	long_description = 'Execute python functions as tasks',
 	url              = 'http://github.com/seomoz/shovel',
